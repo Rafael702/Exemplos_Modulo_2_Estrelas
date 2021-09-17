98c06ee4fd5b39cefb159f3c79d4b5663799c19a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-17 11:30:30 -0300

    Aula sobre classes. Instanciando a classe contato na classe principal.

---
+++ src/br/com/zup/Contato.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+
+public class Contato {
+    String nome;
+    String telefone;
+    String cpf;
+    String email;
+}
---
+++ src/br/com/zup/Main.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Contato objeto_contato = new Contato();
+        System.out.println("Atributo sem preechimento:");
+        System.out.println(objeto_contato.nome);
+
+        System.out.println("Atributo com Preenchimento:");
+        objeto_contato.nome = "Rafão";
+        System.out.println(objeto_contato.nome);
+
+    }
+}

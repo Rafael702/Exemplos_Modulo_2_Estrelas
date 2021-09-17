323eb7f83724c7c61927f1fe4ca46e866540d809
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-17 12:04:37 -0300

    Testes da linha 10 até a linha 15. Classe Main.

--- src/br/com/zup/exemploanimal/Main.java
+++ src/br/com/zup/exemploanimal/Main.java
@@ -5,7 +5,12 @@ public class Main {
         Baleia  freewilly = new Baleia(80, 500, "fundo do mar", "Orca");
         Cachorro betoven = new Cachorro("puddle", "pequeno porte",2, 50 );
         Gato frajola = new Gato("persa", "branca", 8, 50);
-        Papagaio loro = new Papagaio("verde",10, false, 70 );
+        Papagaio loro = new Papagaio("verde",10, true, 70 );
 
+        if(loro.palavrao == false){
+            System.out.println("Não fala palavrão.");
+        }else{
+            System.out.println("Fala palavrão.");
+        }
     }
 }

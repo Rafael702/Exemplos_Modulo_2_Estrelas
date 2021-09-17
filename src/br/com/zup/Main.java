27e6797676b3eb4b54630df8b82587e009c786bd
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-17 12:10:19 -0300

    Corrigindo Conflitos.

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.zup;
 
 public class Main {
@@ -12,32 +11,5 @@ public class Main {
         System.out.println(objeto_contato.email);
         System.out.println(objeto_contato.cpf);
         System.out.println(objeto_contato.idade);
-
-=======
-
-//Formatações
-package br.com.zup;
-
-import java.util.Scanner;
-
-public class Main {
-
-
-    public static void main(String[] args) {
-// 2. instanciando e criando um objeto Scanner
-        Scanner ler = new Scanner(System.in);
-
-        int i, n;
-
-        System.out.printf("Informe o número para a tabuada:\n");
-        n = ler.nextInt(); // 3.1 entrada de dados (lendo um valor inteiro)
-
-        System.out.printf("\n" + "--Resultado--+\n");
-        for (i = 1; i <= 10; i++) {
-            System.out.printf("| %2d * %d = %2d |\n", i, n, (i * n));
-        }
-        System.out.printf("+-------------+\n");
->>>>>>> 6adb1f5a9cb18f908eb90059cef264c634ed6d0e
-
     }
 }
